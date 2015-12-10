e4eb512c718aba33977918b589e7e59df00a530e
Jayden Windle <jaydenwindle@gmail.com>
2015-12-10 14:20:45 -0500

    Setting up jade

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var request = require('request');
 var qs = require('querystring');
-var app = express();
 
+// Setting up app
+var app = express();
+app.set('view engine', 'jade');
+
+// Routes
 app.get('/', function(req, res) {
     res.send("hello world")
 });
 
 app.get('/:book/:chapter/:verse', function(req, res) {
+    var passage = req.params.book + ' ' + req.params.chapter + ':' + req.params.verse
     var query = {
         'key': 'IP',
-        'passage': req.params.book + ' ' + req.params.chapter + ' ' + req.params.verse,
+        'passage': passage,
         'include-footnotes': false,
         'include-short-copyright': false,
         'include-audio-link': false
@@ -19,11 +24,12 @@ app.get('/:book/:chapter/:verse', function(req, res) {
 
     request('http://www.esvapi.org/v2/rest/passageQuery?' + querystring, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            res.send(body)
+            res.render('verse', {pageTitle: passage, bible: body})
         }
     });
 });
 
+// Start app
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
