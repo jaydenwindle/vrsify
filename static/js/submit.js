53b96fd3425d4249744f24fd4cd7c191d3a7f604
Jayden Windle <jaydenwindle@gmail.com>
2015-12-11 13:40:14 -0500

    verse verification on main page

--- static/js/submit.js
+++ static/js/submit.js
@@ -6,28 +6,14 @@ $(document).ready(function() {
         var verse = $('input[name=verse]').val();
 
         url = '/' + book + '/' + chapter + '/' + verse
-        console.log(isValidQuery(url));
-        if (isValidQuery(url)) {
-            window.location.href = url
-        } else {
-            alert("Sorry, that's not a bible verse.")
-        }
 
-        // console.log(url);
-        // window.location.href = url
+        $.get(url, function(data) {
+            if (String(data).indexOf('ERROR:') > -1) {
+                alert("Sorry, that's not a bible verse.");
+            } else {
+                window.location.href = url;
+            }
+        });
+        
     });
 });
-
-function isValidQuery(url) {
-    var isValid;
-    $.get(url, function(data) {
-        if (String(data).indexOf('ERROR:') > -1) {
-            // console.log(false);
-            isValid = false;
-        } else {
-            // console.log(true);
-            isValid = true;
-        }
-    });
-    return isValid;
-}
