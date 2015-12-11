c6a04b6311b7eeda3c44f863e49fc2e9ee359148
Jayden Windle <jaydenwindle@gmail.com>
2015-12-10 22:08:58 -0500

    openshift fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 app.set('view engine', 'jade');
 app.use(express.static('static'));
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 // Data
