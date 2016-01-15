24483bc019539ecd24b66aebdef938087fe98dda
Jayden Windle <jaydenwindle@gmail.com>
2016-01-15 17:28:31 -0500

    Fiddling with highlighting functionality Not working totally yet, but its getting there :)

--- static/js/highlight.js
+++ static/js/highlight.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
-    
+
+    // handle highlights in url
+    var hl = getQueryList().hl;
+    var v, s, e;
+    for (var i = 0; i < hl.length; i++) {
+        v = hl[i].split(':')[0];
+        s = hl[i].split(':')[1].split('-')[0];
+        e = hl[i].split(':')[1].split('-')[1];
+        console.log(v, s, e);
+    }
+
+    // handle user highlighting
+    $('.esv-text').mouseup(function() {
+        var sel = window.getSelection();
+        var range = sel.getRangeAt(0);
+        var node = document.createElement('span');
+        node.className = 'highlight';
+        range.surroundContents(node)
+        // if (sel.toString() != '') {
+        //     // something is highlighted
+        //     console.log(range.startContainer, range.startOffset, range.endOffset);
+        //     highlight(range.startContainer.parentNode.parentNode.id, range.endContainer.parentNode.parentNode.id, range.startOffset, range.endOffset);
+        //     document.getSelection().empty();
+        // } else {
+        //     // nothing selected, clear all highlights
+        //     console.log($('.highlight').children()[0]);
+        // }
+    })
 })
 
 function getQueryList(){
@@ -15,10 +42,43 @@ function getQueryList(){
     return queryList;
 }
 
-function escapeId(id) {
-    id = id.replace(/\./g, '\\.');
-    id = id.replace(/\-/g, '\\-');
-    return id;
-}
+function highlight(startId, endId, start, end) {
+    var verse, vs, ve
+    if (startId != endId) {
+        vs = $('#' + startId).children('.verse-text').html();
+        vs = vs.slice(0, start) + '<span class="highlight">' + vs.slice(start) + '</span>';
+        console.log(vs);
+        $('#' + startId).children('.verse-text').html(vs);
 
-function highlight(hl) {
+        for (var i = Number(startId) + 1; i < endId; i++) {
+            verse = $('#' + i).children('.verse-text').html();
+            verse = '<span class="highlight">' + verse + '</span>'
+            console.log(verse);
+            $('#' + i).children('.verse-text').html(verse);
+        }
+
+        ve = $('#' + endId).children('.verse-text').html();
+        ve = '<span class="highlight">' + ve.slice(0, end) + '</span>' + ve.slice(end);
+        $('#' + endId).children('.verse-text').html(ve);
+        console.log(ve);
+    } else {
+        vs = $('#' + startId).children('.verse-text').html();
+        vs = vs.slice(0, start) + '<span class="highlight">' + vs.slice(start, end) + '</span>' + vs.slice(end);
+        console.log(vs);
+        $('#' + startId).children('.verse-text').html(vs);
+    }
+
+
+
+    // txt = verse.html();
+    // before = txt.slice(0, start);
+    // if (end > txt.length) {
+    //     end = txt.length;
+    //     after = '';
+    // } else {
+    //     after = txt.slice(end);
+    // }
+    // hl = txt.slice(start, end);
+    //
+    // verse.html(before + '<span class="highlight">' + hl + '</span>' + after);
+}
